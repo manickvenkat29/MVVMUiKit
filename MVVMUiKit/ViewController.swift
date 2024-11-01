1627b7401982e8062508577a111c46b3fcd438ed
manickvenkat29 <manickvenkat29@gmail.com>
2024-11-01 15:11:47 +0530

    Initial Commit

---
+++ MVVMUiKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMUiKit
+//
+//  Created by Manickam on 01/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
